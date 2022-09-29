53837681e21f38976d8830b5b6da8055ad5db237
velpuri.madhu.babu <madhu.velpuri.consultant@nielseniq.com>
2022-09-30 00:28:01 +0530

    sample commit

--- src/main/java/tricks/FirstLetterCaps.java
+++ src/main/java/tricks/FirstLetterCaps.java
@@ -5,7 +5,6 @@ public class FirstLetterCaps {
     public static void main(String[] args) {
 
         String s = "madhu";
-
         System.out.println(s.substring(0,1).toUpperCase()+s.substring(1));
     }
 }
